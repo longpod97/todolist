e2874c5041bd91eb3e0d450a74e26d0594857f84
longpod97 <175112945+longpod97@users.noreply.github.com>
2024-10-17 18:39:39 +0800

    CSS Complete

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import "./styles.css";
+
+const addProject = document.getElementById("add-proj-bttn");
+const projModal = document.getElementById("project-modal");
+const projSubmit = document.getElementById("proj-submit");
+const projCancel = document.getElementById("proj-cancel")
+
+addProject.addEventListener("click", () => {
+    projModal.showModal();
+})
+
+projSubmit.addEventListener("click", () => {
+    projModal.closeModal();
+});
+
+projCancel.addEventListener("click", () => {
+    closeModal();
+});
